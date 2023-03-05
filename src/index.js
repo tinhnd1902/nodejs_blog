d4d04dd7e43a5587157a177d182d9126c863af1d
tinhnd <tinhnd1902@gmail.com>
2023-03-05 21:39:18 +0700

    MVC Router & Controller

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ const hbs = require('express-handlebars');
 const app = express();
 const port = 3000;
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
     extended: true
@@ -22,21 +24,8 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-    // res.send('Hello World!')
-    res.render('home')
-});
-app.get('/news', (req, res) => {
-    res.render('news')
-});
-app.get('/search', (req, res) => {
-    console.log('search: ', req.query)
-    res.render('search')
-});
-app.post('/search', (req, res) => {
-    console.log("req: ", req.body)
-    res.send('')
-});
+//route init
+route(app)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
